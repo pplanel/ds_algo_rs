794665ac45387fd219c9e672f39e778ec4ed11e1
pplanel@superluminal.com.br <Pedro Gabriel Planel>
2022-04-24 23:43:01 -0300

    refactor begin

--- src/binary_tree/mod.rs
+++ src/binary_tree/mod.rs
@@ -1,246 +1,173 @@
-use std::collections::VecDeque;
+use std::cmp;
 
-pub type OptionNodeRef<T> = Option<Box<Node<T>>>;
-pub type NodeRef<T> = Box<Node<T>>;
-
-#[derive(Debug, Default, PartialEq, PartialOrd, Clone)]
-pub struct Node<T>
-where
-    T: PartialOrd + PartialEq + Clone,
-{
-    pub value: T,
-    pub left: OptionNodeRef<T>,
-    pub right: OptionNodeRef<T>,
+#[derive(Debug, Eq, PartialOrd, Ord)]
+pub enum BinarySearchTree<T: Ord> {
+    Leaf {
+        value: T,
+        left: Box<BinarySearchTree<T>>,
+        right: Box<BinarySearchTree<T>>,
+    },
+    Empty,
 }
 
-impl From<i32> for Node<i32> {
-    fn from(val: i32) -> Self {
-        Node::<i32>::new(val)
+impl<T: Ord> Default for BinarySearchTree<T> {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
-impl<T> Node<T>
-where
-    T: PartialEq + PartialOrd + Clone,
-{
-    pub fn new(value: T) -> Node<T> {
-        Node::<T> {
-            value,
-            left: None,
-            right: None,
+impl<T: Ord> PartialEq for BinarySearchTree<T> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (
+                Self::Leaf {
+                    value: l_value,
+                    left: l_left,
+                    right: l_right,
+                },
+                Self::Leaf {
+                    value: r_value,
+                    left: r_left,
+                    right: r_right,
+                },
+            ) => l_value == r_value && l_left == r_left && l_right == r_right,
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
+        }
+    }
+}
+
+impl<T: Ord> BinarySearchTree<T> {
+    pub fn new() -> Self {
+        BinarySearchTree::Empty
+    }
+
+    pub fn height(self) -> usize {
+        if let BinarySearchTree::Leaf {
+            value: _,
+            left,
+            right,
+        } = self
+        {
+            let count_left = Self::height(*left);
+            let count_right = Self::height(*right);
+            let height = cmp::max(count_left, count_right);
+            height + 1
+        } else {
+            0
         }
     }
 
-    pub fn left<N>(mut self, node: N) -> Self
-    where
-        N: Into<Node<T>>,
-        T: std::fmt::Debug,
-    {
-        self.left = Some(Box::new(node.into()));
-        self
+    pub fn create(value: T) -> Self {
+        BinarySearchTree::Leaf {
+            value,
+            left: Box::new(BinarySearchTree::Empty),
+            right: Box::new(BinarySearchTree::Empty),
+        }
     }
 
-    pub fn right<N>(mut self, node: N) -> Self
-    where
-        N: Into<Node<T>>,
-        T: std::fmt::Debug,
-    {
-        self.right = Some(Box::new(node.into()));
-        self
-    }
-}
-
-pub fn root(value: i32) -> Node<i32> {
-    Node::new(value)
-}
-
-impl<T> Node<T>
-where
-    T: PartialOrd + PartialEq + Clone,
-{
-    pub fn create_unbalanced_tree() -> NodeRef<i32> {
-        let root = root(1).left(root(2).left(4).right(5)).right(3);
-        Box::new(root)
-    }
-
-    pub fn insert(&mut self, value: T) {
-        let mut queue: VecDeque<&mut Node<T>> = VecDeque::new();
-        queue.push_front(self);
-        loop {
-            let Node {
+    pub fn insert(&mut self, new_value: T) {
+        match self {
+            BinarySearchTree::Leaf {
+                ref value,
                 ref mut left,
                 ref mut right,
-                ..
-            } = queue.pop_back().unwrap();
-            match left {
-                Some(node) => queue.push_front(node),
-                None => {
-                    *left = Some(Box::new(Node::<T>::new(value)));
-                    return;
-                }
-            }
-            match right {
-                Some(node) => queue.push_front(node),
-                None => {
-                    *right = Some(Box::new(Node::<T>::new(value)));
-                    return;
-                }
-            }
+            } => match new_value.cmp(value) {
+                std::cmp::Ordering::Less => left.insert(new_value),
+                std::cmp::Ordering::Greater => right.insert(new_value),
+                std::cmp::Ordering::Equal => (),
+            },
+            BinarySearchTree::Empty => *self = BinarySearchTree::create(new_value),
         }
     }
 
-    pub fn search(self, value: T) -> OptionNodeRef<T> {
-        if self.value == value {
-            return Some(Box::new(self));
+    pub fn sorted_values(&self) -> Vec<&T> {
+        let mut values = Vec::new();
+
+        match self {
+            BinarySearchTree::Leaf {
+                ref value,
+                ref left,
+                ref right,
+            } => {
+                values.extend(left.sorted_values());
+                values.push(value);
+                values.extend(right.sorted_values());
+            }
+            BinarySearchTree::Empty => {}
         }
-        let curr = self;
-        if curr.value < value {
-            if let Some(right) = curr.right {
-                Self::search(*right, value.clone())
-            } else {
-                None
-            };
-        }
-        if let Some(left) = curr.left {
-            Self::search(*left, value)
-        } else {
-            panic!("null");
-        }
+
+        values
     }
 }
 
-pub mod stack_traversal {
-    use super::NodeRef;
-
-    //pub fn post_order(root: &NodeRef<i32>, buffer: &mut Vec<i32>) {}
-    pub fn in_order(root: &NodeRef<i32>, buffer: &mut Vec<i32>) {
-        let mut stack = Vec::<&NodeRef<i32>>::new();
-        let mut curr = Some(root);
-
-        while curr.is_some() || !stack.is_empty() {
-            while curr.is_some() {
-                stack.push(curr.unwrap());
-                curr = curr.unwrap().left.as_ref();
-            }
-
-            curr = stack.pop();
-            buffer.push(curr.unwrap().value);
-            curr = curr.unwrap().right.as_ref();
-        }
-    }
-}
-
-pub mod traversal {
-    use super::OptionNodeRef;
-
-    pub fn post_order(root: OptionNodeRef<i32>, buffer: &mut Vec<i32>) {
-        // TODO(pplanel): how to get the len of tree by level
-        match root {
-            Some(node) => {
-                post_order(node.left, buffer);
-                post_order(node.right, buffer);
-                buffer.push(node.value);
-            }
-            None => {}
-        }
-    }
-
-    pub fn pre_order(root: OptionNodeRef<i32>, buffer: &mut Vec<i32>) {
-        // TODO(pplanel): how to get the len of tree by level
-        match root {
-            Some(node) => {
-                buffer.push(node.value);
-                pre_order(node.left, buffer);
-                pre_order(node.right, buffer);
-            }
-            None => {}
-        }
-    }
-
-    pub fn in_order(root: OptionNodeRef<i32>, buffer: &mut Vec<i32>) {
-        // TODO(pplanel): how to get the len of tree by level
-        match root {
-            Some(node) => {
-                in_order(node.left, buffer);
-                buffer.push(node.value);
-                in_order(node.right, buffer);
-            }
-            None => {}
-        }
+#[allow(dead_code)]
+fn print_tree<T>(root: &BinarySearchTree<T>)
+where
+    T: Ord + std::fmt::Display,
+{
+    if let BinarySearchTree::Leaf { value, left, right } = root {
+        print_tree(left);
+        println!("{}", value);
+        print_tree(right);
     }
 }
 
 #[cfg(test)]
-mod test_binary_tree_traversal {
-    use crate::binary_tree::{root, stack_traversal, traversal, Node};
+mod test_bst {
+    use crate::binary_tree::BinarySearchTree;
+
+    use super::print_tree;
 
     #[test]
-    fn test_search_fb() {
-        let mut tree = Node::new(1);
-        tree.insert(2);
-        tree.insert(3);
-        tree.insert(4);
-        tree.insert(5);
-        tree.insert(6);
-        if let Some(node) = tree.search(6) {
-            assert_eq!(*node, root(6));
+    fn test_new_bst() {
+        assert_eq!(BinarySearchTree::<i32>::new(), BinarySearchTree::Empty);
+    }
+
+    #[test]
+    fn test_create_bst() {
+        let root = BinarySearchTree::create(1_usize);
+        match root {
+            BinarySearchTree::Leaf { value, left, right } => {
+                assert_eq!(value, 1_usize);
+                assert_eq!(*left, BinarySearchTree::Empty);
+                assert_eq!(*right, BinarySearchTree::Empty);
+            }
+            BinarySearchTree::Empty => panic!("BinarySearchTree::create returned Empty"),
         }
     }
 
     #[test]
-    fn test_insert_fb() {
-        let mut tree = Node::new(1);
-        tree.insert(2);
-        tree.insert(3);
-        tree.insert(4);
-        tree.insert(5);
-        assert_eq!(tree, root(1).left(root(2).left(4).right(5)).right(3));
-        tree.insert(6);
+    fn test_insert_bst() {
+        let mut root = BinarySearchTree::create(1_usize);
+        root.insert(2_usize);
+        root.insert(3_usize);
+        root.insert(4_usize);
+        root.insert(5_usize);
         assert_eq!(
-            tree,
-            root(1)
-                .left(root(2).left(4).right(5))
-                .right(root(3).left(6))
+            root.sorted_values(),
+            vec![&1_usize, &2_usize, &3_usize, &4_usize, &5_usize]
         );
     }
 
     #[test]
-    fn test_in_order_stack() {
-        let mut buffer = Vec::new();
-        let root = Node::<i32>::create_unbalanced_tree();
-        stack_traversal::in_order(&root, &mut buffer);
-        assert_eq!(buffer, vec![4, 2, 5, 1, 3]);
-    }
-
-    //#[test]
-    //fn test_post_order_stack() {
-    //    let mut buffer = Vec::new();
-    //    let root = Node::<i32>::create_unbalanced_tree();
-    //    stack_traversal::post_order(&root, &mut buffer);
-    //    assert_eq!(buffer, vec![4, 5, 2, 3, 1]);
-    //}
-
-    #[test]
-    fn test_in_order() {
-        let mut buffer = Vec::new();
-        let root = Node::<i32>::create_unbalanced_tree();
-        traversal::in_order(Some(root), &mut buffer);
-        assert_eq!(buffer, vec![4, 2, 5, 1, 3]);
+    fn test_height_bst() {
+        let mut root = BinarySearchTree::create(5);
+        root.insert(3);
+        root.insert(6);
+        root.insert(2);
+        root.insert(4);
+        root.insert(1);
+        assert_eq!(root.height(), 4);
     }
 
     #[test]
-    fn test_pre_order() {
-        let mut buffer = Vec::new();
-        let root = Node::<i32>::create_unbalanced_tree();
-        traversal::pre_order(Some(root), &mut buffer);
-        assert_eq!(buffer, vec![1, 2, 4, 5, 3]);
-    }
-
-    #[test]
-    fn test_post_order() {
-        let mut buffer = Vec::with_capacity(5);
-        let root = Node::<i32>::create_unbalanced_tree();
-        traversal::post_order(Some(root), &mut buffer);
-        assert_eq!(buffer, vec![4, 5, 2, 3, 1]);
+    fn test_print_bst() {
+        let mut root = BinarySearchTree::create(5);
+        root.insert(3);
+        root.insert(6);
+        root.insert(2);
+        root.insert(4);
+        root.insert(1);
+        print_tree(&root)
     }
 }
